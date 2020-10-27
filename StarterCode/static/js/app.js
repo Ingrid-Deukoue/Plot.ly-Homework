43103cfad8ee9962404e9bbf7e6dadf0ad386bbe
deukoue33 <66564798+deukoue33@users.noreply.github.com>
2020-10-27 08:48:56 -0400

    remove grading and push first JS

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,118 @@
+// create function to create charts & panel
+function populatePage(subjectID) {
+ d3.json("samples.json").then((data) => {
+     var samples = data.samples;
+     var resultArray = samples.filter(s => s.id == subjectID);
+     var result = resultArray[0];
+     var sampleValues = result.sample_values
+     var otuIDs = result.otu_ids;
+     var otuLabels = result.otu_labels;
+     var metaData = data.metadata;
+     var metaArray = metaData.filter(md => md.id == subjectID);
+     var metaResult = metaArray[0];
+     var washFrequency = metaResult.wfreq;
+             
+     // Draw the bar chart
+     var barData = [{
+         x: sampleValues.slice(0, 10).reverse(),
+         y: otuIDs.slice(0, 10).map(otuID => `OTU ${otuID} `).reverse(),
+         text: otuLabels.slice(0, 10).reverse(),
+         type: "bar",
+         orientation: "h"
+     }];
+     var barLayout = {
+             margin: {t: 30, l: 150}
+     };
+     Plotly.newPlot("bar", barData, barLayout);
+
+     // Draw the bubble graph
+     var bubbleData = [{
+         x: otuIDs,
+         y: sampleValues,
+         text: otuLabels,
+         type: "bubble",
+         mode: "markers",
+         marker: {
+             size: sampleValues,
+             color: otuIDs,
+             colorscale: "Earth"
+         }
+     }];
+     var bubbleLayout = {
+         margin: {t: 30, l: 150},
+     };
+     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+     // Populate the panel with demographic info
+     var panel = d3.select("#sample-metadata");
+     panel.html("");
+     Object.entries(metaResult).forEach(([key, value]) => {
+         panel.append("h6").text(`${key}: ${value}`);
+     });
+
+     // **************BONUS: Create gauge****************
+     var gaugeData = [{
+         type: "indicator",
+         mode: "gauge+number",
+         value: washFrequency,
+         delta: { reference: 400, increasing: { color: "Purple"} },
+         text: ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9"],
+         gauge: {
+             axis: {range: [0,9], tickwidth: 1, tickcolor: "blue"},
+             bar: {color: "#83406f"},
+             steps: [
+                 {range: [0, 1], color: "#edcce2"},  
+                 {range: [1, 2], color: "#e0bad3"},
+                 {range: [2, 3], color: "#d2a8c4"},
+                 {range: [3, 4], color: "#c596b5"},
+                 {range: [4, 5], color: "#b884a7"},
+                 {range: [5, 6], color: "#aa7399"},  
+                 {range: [6, 7], color: "#9d628b"},
+                 {range: [7, 8], color: "#90517d"},
+                 {range: [8, 9], color: "#83406f"}
+             ]
+         }
+     }]
+
+     var gaugeLayout = { 
+      width: 600, 
+      height: 500, 
+      margin: { t: 0, b: 0 } 
+  };
+
+     Plotly.newPlot("gauge", gaugeData, gaugeLayout);
+ });
+};
+
+// Event Handler for selecting new subject ID in dropdown box
+function optionChanged(newSubjectID) {
+ console.log("User selected subject ID:", newSubjectID);
+ populatePage(newSubjectID);
+};
+
+// Initialization function to draw data for first subject
+function initDashboard() {
+ var selector = d3.select("#selDataset");
+ 
+ // Load the data
+ d3.json("samples.json").then((data) => {
+     console.log(data);
+
+     // Get the subject names
+     var subjectNames = data.names;
+
+     // Populate the selector with the test subject IDs
+     subjectNames.forEach((subjectID) => {
+         selector.append("option")
+             .text(subjectID)
+             .property("value", subjectID);
+     });
+     
+     // Get initial subject ID
+     var subjectID = subjectNames[0];
+     console.log("Initial subject ID:", subjectID);
+     populatePage(subjectID);
+ });
+}
+
+initDashboard();
